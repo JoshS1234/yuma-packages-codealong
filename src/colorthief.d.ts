2db51dcae232c096b6dd9f9c5a7dfda8bdc3305f
JOSH SPENCE <joshua.spence@hotmail.co.uk>
2024-04-03 12:16:11 +0100

    worked on styling and added some functionality

---
+++ src/colorthief.d.ts
@@ -0,0 +1,12 @@
+declare module "colorthief";
+
+type Color = [number, number, number];
+
+type ColorThief = {
+  getColor: (img: HTMLImageElement, quality?: number) => Color;
+  getPalette: (
+    img: HTMLImageElement,
+    quality?: number,
+    colorCount?: number
+  ) => Color[];
+};
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,65 @@
 import "./style.scss";
+import confetti, { Options } from "canvas-confetti";
+import ColorThief from "colorthief";
 
-console.log("Welcome to NPM!");
+const confettiButton = document.querySelector("#confetti-button");
+const dogImage = document.querySelector<HTMLImageElement>("#dog-image");
+const newImageTextbox =
+  document.querySelector<HTMLInputElement>("#new-image-textbox");
+
+if (!confettiButton) {
+  throw new Error("Confetti issue");
+} else if (!dogImage) {
+  throw new Error("dog image issue");
+} else if (!newImageTextbox) {
+  throw new Error("image textbox issue");
+}
+
+const colorThief = new ColorThief();
+//create a function on image load that does the colorthief.getColor
+
+let fireButtonOptions: Options = {
+  particleCount: 80,
+  shapes: ["star"],
+};
+
+const onImageLoad = () => {
+  const color = colorThief.getColor(dogImage);
+  document.body.style.backgroundColor = `rgb(${color[0]},${color[1]},${color[2]})`;
+  confetti();
+};
+
+const fireConfetti = () => {
+  const rect = confettiButton.getBoundingClientRect();
+
+  fireButtonOptions.spread = Math.ceil(Math.random() * 360);
+  fireButtonOptions.angle = Math.ceil(Math.random() * 360);
+  fireButtonOptions.origin = {
+    x: (rect.right + rect.left) / (2 * window.innerWidth),
+    y: (rect.top + rect.bottom) / (2 * window.innerHeight),
+  };
+  confetti(fireButtonOptions);
+};
+
+if (dogImage.complete) {
+  onImageLoad();
+} else {
+  dogImage.addEventListener("load", onImageLoad);
+}
+
+const alterImageURL = (e: Event) => {
+  const target = e.target as HTMLInputElement;
+
+  dogImage.src = target.value;
+  onImageLoad();
+  const palette = colorThief.getPalette(dogImage);
+  const confettiColArr = palette.map((color: Color) => {
+    return `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
+  });
+  fireButtonOptions.colors = confettiColArr;
+
+  confetti(fireButtonOptions);
+};
+
+confettiButton?.addEventListener("click", fireConfetti);
+newImageTextbox.addEventListener("change", alterImageURL);
